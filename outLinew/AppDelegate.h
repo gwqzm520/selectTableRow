251d574e99e9093e64e21766e15378845f481b6f
Shirly Zhu <shirly.zhu@music-group.com>
2017-08-28 13:06:05 +0800

    Initial Commit

---
+++ outLinew/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  outLinew
+//
+//  Created by shirly.zhu on 2017/8/28.
+//  Copyright © 2017年 shirly.zhu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ outLinew/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  outLinew
+//
+//  Created by shirly.zhu on 2017/8/28.
+//  Copyright © 2017年 shirly.zhu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
